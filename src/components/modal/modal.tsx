90457fe3208a691eef1b5bb0bfece9b8c06d906b
jon <jon@onchainedlabs.io>
2023-03-12 09:53:27 -0500

    second commit

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -1,16 +1,12 @@
-import { component$, useBrowserVisibleTask$, useStore } from "@builder.io/qwik";
+import { component$ } from "@builder.io/qwik";
 //is this the props.children issue that was discussed in the solid / marko dev post?
 
-export default component$((props) => {
-  const { onClose } = props;
+export default component$(() => {
   return (
     <div class="absolute top-0 right-0 w-screen h-screen bg-white z-50 flex flex-col gap-4 p-4 text-slate-900">
       <div class="flex items-center justify-between pb-4 border-b">
         <h1 class="font-bold text-4xl">CART</h1>
-        <i
-          class="fa-solid fa-xmark justify-center cursor-pointer"
-          onClick$={onClose}
-        ></i>
+        <i class="fa-solid fa-xmark justify-center cursor-pointer"></i>
       </div>
     </div>
   );
